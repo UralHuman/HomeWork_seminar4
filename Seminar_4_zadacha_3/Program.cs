945f2ce5594344dab00a736c7967903be54c61aa
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-26 21:47:30 +0500

    create code_3

---
+++ Seminar_4_zadacha_3/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.Write("Введите колличество элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());;
+int [] array = new int[n];
+
+// Random ran = new Random();
+
+for (int i=0; i< n; i++)
+{
+ Console.Write("Введите элемент №{0}:\r\n        ", i + 1);
+// Random ran = new Random();
+// array[i] = ran.Next();
+
+
+ array[i]=Convert.ToInt32(Console.ReadLine());;
+}
+System.Console.WriteLine("Вы ввели следующие символы: ");
+
+for(int i =0;i<n;i++)
+{
+    
+    System.Console.WriteLine(array[i]);
+}
