dcf8c4c8775c8f68178659ddf0cd38c265fb6637
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-26 17:20:40 +0500

    create code_2

---
+++ Seminar_4_zadacha_2/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write(">>> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int sum =0;
+ while (a>0)
+ {
+    int digit = a%10;
+    sum = sum + digit;
+    a = a/10;
+ }
+ System.Console.WriteLine($"Сумма цифр = {sum}");
+
+
