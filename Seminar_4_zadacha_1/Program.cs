ef3b97f0a22aec4aaa6098cfe26582952fed75f2
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-01-26 11:56:22 +0500

    create code1.0

---
+++ Seminar_4_zadacha_1/Program.cs
@@ -0,0 +1,22 @@
+﻿int MaPow()
+{
+System.Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int count = a;
+
+int i = 1;
+
+while (i<b)
+{
+    
+    a = a*count;
+    i++;
+    
+}
+return a;
+}
+int c = MaPow();
+System.Console.Write(c);
